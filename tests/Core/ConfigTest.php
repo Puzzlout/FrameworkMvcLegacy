3dd57cdc3ec5522424af07ffacba6d640d79baaa
Jérémie Litzler <webdev.jl@gmail.com>
2016-01-13 07:19:41 +0100

    #21

---
+++ tests/Core/ConfigTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 
+ * @since Test Suite v1.0.0
+ */
+
+namespace WebDevJL\Framework\Tests\Core;
+
+use WebDevJL\Framework\Core\Config;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase {
+
+  protected $app;
+
+  /**
+   * Initialize the app object.
+   */
+  protected function setUp()
+  {
+      $this->app = new \WebDevJL\Framework\Tests\TestApplication();
+  }
+  
+  /**
+   * This method is generated.
+   */
+  public function testInstanceIsCorrect()
+  {
+    $this->assertNotNull($this->app);
+    $result = new Config($this->app);
+    $this->assertInstanceOf('WebDevJL\Framework\Core\Config', $result);
+  }
+  
+  //Write the next tests below...
+  
+}
